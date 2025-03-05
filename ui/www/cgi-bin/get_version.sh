7f7712f26f1e33c5d2ddc6609c8eb4723b077473
Gugun <nandakanaksasak@gmail.com>
2025-03-06 00:51:22 +0800

    Perbaikan Install dan Service

--- ui/www/cgi-bin/get_version.sh
+++ ui/www/cgi-bin/get_version.sh
@@ -1,5 +1,11 @@
 #!/system/bin/sh
 
+# URL file version.json di GitHub
+GITHUB_URL="https://raw.githubusercontent.com/gugun09/v2ray-monitor-module/main/version.json"
+
+# Menampilkan header JSON
 echo "Content-Type: application/json"
 echo ""
-echo '{"devName": "Gugun09", "appVersion": "1.2.0", "codeVersion": "20250304"}'
+
+# Ambil langsung dari GitHub
+curl -s "$GITHUB_URL"
